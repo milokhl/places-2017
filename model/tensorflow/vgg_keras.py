5925c0f0339c96ee247cfbf3dad1b863829681f6
Milo Knowles <mknowles@mit.edu>
2017-10-15 19:22:15 -0400

    Add validation stats and saving

--- model/tensorflow/vgg_keras.py
+++ model/tensorflow/vgg_keras.py
@@ -16,21 +16,24 @@ import tensorflow as tf
 import time
 
 # Dataset Parameters
-batch_size = 1024
-load_size = 256
-fine_size = 224
+batch_size = 1024 # How many images are used for one training iteration.
+gpu_size = 12 # How many training examples can fit on the GPU?
+load_size = 256 # Size of the images on disk.
+fine_size = 224 # Size we want the images to be for training.
 c = 3
-data_mean = np.asarray([0.45834960097,0.44674252445,0.41352266842])
+data_mean = np.asarray([0.45834960097,0.44674252445,0.41352266842]) # Leave this as is.
 num_classes = 100
 
 # Training Parameters
-learning_rate = 0.001
 dropout = 0.5 # Dropout, probability to keep units
 training_iters = 100000
-step_display = 1
-step_save = 10000
-path_save = 'vgg16'
+save_iter = 100
+validate_iter = 100
+path_save = os.path.join('checkpoints/', 'vgg16_keras_latest.h5')
 start_from = ''
+
+if not os.path.exists('./checkpoints'):
+    os.mkdir('./checkpoints')
 
 # Construct dataloader
 opt_data_train = {
@@ -55,6 +58,8 @@ opt_data_val = {
 loader_train = DataLoaderDisk(**opt_data_train)
 loader_val = DataLoaderDisk(**opt_data_val)
 
+# Define the model.
+
 # weights: 'None' to train on randomly initialized weights, or 'imagenet'
 # include_top: whether to include the top 3 fully connected layers of the model
 # pooling: 'avg' uses GlobalAveragePooling2D, which returns a 2D tensor
@@ -64,6 +69,7 @@ x = Dense(1024, activation='relu')(x)
 x = Dense(1024, activation='relu')(x)
 predictions = Dense(num_classes, activation='softmax')(x)
 
+learning_rate = 0.001
 optimizer = Adam(lr=learning_rate)
 model = Model(inputs=base_model.input, outputs=predictions)
 model.compile(optimizer=optimizer,
@@ -76,15 +82,27 @@ def convert_labels_categorical(labels_batch):
         labels[i][int(labels_batch[i])] = 1
     return labels
 
-# At each iteration, load a batch into memory
-# And train for some number of epochs on it
-gpu_size=12 # largest batch we can fit on GPU
+# Main training loop.
 for t_iter in range(training_iters):
+
+    # Train.
     images_batch, labels_batch = loader_train.next_batch(batch_size)
     labels = convert_labels_categorical(labels_batch) # need to make this into a one-hot vector
     model.fit(x=images_batch, y=labels, batch_size=gpu_size, epochs=1, verbose=1)
 
-    # At the end of this training iteration, test on the validation set.
-    images_val, labels_val = loader_val.next_batch(batch_size)
-    labels_val_categorical = convert_labels_categorical(labels_val)
-    model.evaluate(x=images_val, y=labels_val_categorical, batch_size=gpu_size, verbose=1)
+    # Show validation stats periodically.
+    if t_iter % validate_iter == 0:
+        # At the end of this training iteration, test on the validation set.
+        images_val, labels_val = loader_val.next_batch(batch_size)
+        labels_val_categorical = convert_labels_categorical(labels_val)
+        validation_stats = model.evaluate(x=images_val, y=labels_val_categorical, batch_size=gpu_size, verbose=1)
+        print('---- Validation results: -----')
+        print('Loss: %f Top5: %f Top1: %f' % (validation_stats[0], validation_stats[1], validation_stats[2]))
+        print('------------------------------')
+    
+    # Save periodically.
+    if t_iter % save_iter == 0:
+        model.save(path_save)
+
+# Do a final save.
+model.save(path_save)
