17adfb61cab0b1727d94025feb109af158993453
Milo Knowles <mknowles@mit.edu>
2017-11-20 19:06:01 -0500

    Ensemble working

--- model/pytorch/ensemble.py
+++ model/pytorch/ensemble.py
@@ -13,63 +13,65 @@ import torch
 from PIL import Image
 import torchvision.transforms as transforms
 from torch.utils.data import Dataset, DataLoader
+from torch.autograd import Variable
 
 import vgg_pytorch as VGG
-from miniplaces_capsnet import PlacesCapsuleNet
+from miniplaces_capsnet3 import PlacesCapsuleNet
 
 from miniplaces_dataset import *
 from utils import accuracy, AverageMeter, save_checkpoint, log
 
 def main():
-	BATCH_SIZE = 64
-	VGG_LOAD_EPOCH = './final/model_best.pth.tar'
-	CAPSNET_LOAD_EPOCH = './final/epoch_16.pt'
+  BATCH_SIZE = 128
+  VGG_LOAD_EPOCH = './final/model_best.pth.tar'
+  CAPSNET_LOAD_EPOCH = './final/epoch_16.pt'
 
-	# Apply a series of transformations to the input data.
-	DATA_MEAN = (0.45834960097, 0.44674252445, 0.41352266842)
-	DATA_STD = (0.229, 0.224, 0.225)
-	CROP_SIZE = 120
-	batch_size = 200
+  # Apply a series of transformations to the input data.
+  DATA_MEAN = (0.45834960097, 0.44674252445, 0.41352266842)
+  DATA_STD = (0.229, 0.224, 0.225)
+  CROP_SIZE = 128
 
-	# Set up VGG16
-	model_vgg16 = VGG.vgg16(num_classes=100, dropout=0.5, light=True)
-	model_vgg16.features = torch.nn.DataParallel(model_vgg16.features)
-	model_vgg16.cuda()
-	model_vgg16.eval()
+  # Set up VGG16
+  model_vgg16 = VGG.vgg16(num_classes=100, dropout=0.5, light=True)
+  model_vgg16.features = torch.nn.DataParallel(model_vgg16.features)
+  model_vgg16.cuda()
 
-	print("Loading VGG checkpoint '{}'".format(VGG_LOAD_EPOCH))
-	checkpoint = torch.load(VGG_LOAD_EPOCH)
-	start_epoch = checkpoint['epoch']
-	best_prec1 = checkpoint['best_prec1']
-	model_vgg16.load_state_dict(checkpoint['state_dict']) # Get frozen weights.
+  print("Loading VGG checkpoint '{}'".format(VGG_LOAD_EPOCH))
+  checkpoint = torch.load(VGG_LOAD_EPOCH)
+  start_epoch = checkpoint['epoch']
+  best_prec1 = checkpoint['best_prec1']
+  model_vgg16.load_state_dict(checkpoint['state_dict']) # Get frozen weights.
 
-	# Set up the PlacesCapsuleNet
-	model_capsnet = PlacesCapsuleNet()
-	print('Loading capsule net model from epoch:', LOAD_EPOCH)
-	model_capsnet.load_state_dict(torch.load(LOAD_EPOCH))
-	model_capsnet.cuda()
-	model_capsnet.eval()
+  # Set up the PlacesCapsuleNet
+  model_capsnet = PlacesCapsuleNet()
+  print('Loading capsule net model from epoch:', CAPSNET_LOAD_EPOCH)
+  state_dict = torch.load(CAPSNET_LOAD_EPOCH)
+  model_capsnet.load_state_dict(state_dict)
+  model_capsnet.cuda()
 
-	print('Batch size:', batch_size)
-	print('Crop size:', CROP_SIZE)
+  print('Batch size:', BATCH_SIZE)
+  print('Crop size:', CROP_SIZE)
 
-	# Load in the validation set.
-	val_set = MiniPlacesDataset(os.path.abspath('./../../data/val.txt'),
-	                            os.path.abspath('./../../data/images/'),
-	                            transform=transforms.Compose([
-	                            transforms.CenterCrop(CROP_SIZE),
-	                            transforms.ToTensor(),
-	                            transforms.Normalize(DATA_MEAN, DATA_STD)]))
+  # Load in the validation set.
+  val_set = MiniPlacesDataset(os.path.abspath('./../../data/val.txt'),
+                              os.path.abspath('./../../data/images/'),
+                              transform=transforms.Compose([
+                              transforms.CenterCrop(CROP_SIZE),
+                              transforms.ToTensor(),
+                              transforms.Normalize(DATA_MEAN, DATA_STD)]))
 
-	val_loader = torch.utils.data.DataLoader(training_set, batch_size=batch_size, shuffle=True, num_workers=4)
+  val_loader = torch.utils.data.DataLoader(val_set, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
 
-	validate(val_loader, [model_vgg16, model_capsnet], print_freq=10)
+  validate(val_loader, [model_vgg16, model_capsnet], print_freq=10)
 
 
 def validate(val_loader, models, print_freq=1):
+  for model in models:
+    model.eval()
+
   print('Starting validation of ensemble!')
-  model1_weight = 0.5
-  model2_weight = 0.5
+  model1_weight = torch.Tensor([0.5]).cuda()
+  model2_weight = torch.Tensor([0.5]).cuda()
 
   batch_time = AverageMeter()
   top1_ensemble = AverageMeter()
@@ -84,47 +86,41 @@ def validate(val_loader, models, print_freq=1):
   end = time.time()
   for i, (input, target) in enumerate(val_loader):
       target = target.cuda(async=True)
-      input_var = torch.autograd.Variable(input, volatile=True)
-      target_var = torch.autograd.Variable(target, volatile=True)
+      input_var = Variable(input, volatile=True).cuda()
+      target_var = Variable(target, volatile=True)
 
       # Get outputs from each model in ensemble.
       outputs = [model(input_var) for model in models]
 
       # Weight each model and combine outputs.
-      ensemble_output = model1_weight * outputs[0] + model2_weight * outputs[1]
+      ensemble_output = model1_weight * outputs[0].data + model2_weight * outputs[1][0].data # Add extra index to caps net because of reconstruction
 
       # Measure accuracies.
-      prec1 = [accuracy(output.data, target, topk=(1)) for output in outputs]
-      prec5 = [accuracy(output.data, target, topk=(5)) for output in outputs]
+      prec1 = [accuracy(outputs[0].data, target, topk=(1,)), accuracy(outputs[1][0].data, target, topk=(1,))]
+      prec5 = [accuracy(outputs[0].data, target, topk=(5,)), accuracy(outputs[1][0].data, target, topk=(5,))]
       top1_model1.update(prec1[0][0], input.size(0))
       top1_model2.update(prec1[1][0], input.size(0))
       top5_model1.update(prec5[0][0], input.size(0))
       top5_model2.update(prec5[1][0], input.size(0))
 
       # Get the enemble accuracies.
-      prec1_ensemble, prec5_ensemble = accuracy(ensemble_output.data, target, topk=(1,5))
-      top1_ensemble.update(prec1_ensemble[0], input.size[0])
-      top5_ensemble.update(prec5_ensemble[0], input_size[0])
+      prec1_ensemble, prec5_ensemble = accuracy(ensemble_output, target, topk=(1,5))
+      top1_ensemble.update(prec1_ensemble, input.size(0))
+      top5_ensemble.update(prec5_ensemble, input.size(0))
 
       # Measure time.
       batch_time.update(time.time() - end)
       end = time.time()
 
+      print_str = 'Validation: [%d/%d] \t Prec1(ens): %f (%f) Prec5(ens): %f (%f) \n' \
+                  % (i, len(val_loader), top1_ensemble.val[0], top1_ensemble.avg[0], top5_ensemble.val[0], top5_ensemble.avg[0])
+      print_str += 'Prec1(m1): %f (%f) Prec5(m1): %f (%f) Prec1(m2): %f (%f) Prec5(m2): %f (%f)'\
+                  % (top1_model1.val[0], top1_model1.avg[0], top5_model2.val[0], top5_model1.avg[0],
+                    top1_model2.val[0], top1_model2.avg[0], top5_model2.val[0], top5_model2.avg[0])
+
       # Print statistacks.
       if i % print_freq == 0:
-          print_str = 'Validation: [{0}/{1}]\t'\
-            'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'\
-            'Prec@1 (ens) {top1_ensemble.val:.3f} ({top1_ensemble.avg:.3f})\t'\
-            'Prec@5 (ens) {top5_ensemble.val:.3f} ({top5_ensemble.avg:.3f})\t \
-            Prec@1 (1) {top1_model1.val:.3f} ({top1_model1.avg:.3f})\t \
-            Prec@5 (1) {top5_model1.val:.3f} ({top5_model1.avg:.3f})\t \
-            Prec@1 (2) {top1_model2.val:.3f} ({top1_model2.avg:.3f})\t \
-            Prec@5 (2) {top5_model2.val:.3f} ({top5_model2.avg:.3f})\t'.format(
-             i, len(val_loader), batch_time=batch_time,
-             top1_ensemble=top1_ensemble, top5_ensemble=top5_ensemble,
-             top1_model1=top1_model1, top5_model1=top5_model1,
-             top1_model2=top1_model2, top5_model2=top5_model2)
-          print(print_str)
+        print(print_str)
 
   print_str = ' * Prec@1 {top1.avg:.3f} Prec@5 {top5.avg:.3f}'.format(top1=top1_ensemble, top5=top5_ensemble)
   print(print_str)
@@ -133,4 +129,4 @@ def validate(val_loader, models, print_freq=1):
   return top1_ensemble.avg, top5_ensemble.avg
 
 if __name__ == '__main__':
-	main()
+  main()
