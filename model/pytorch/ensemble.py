07ff665d6fd1e533c367dc5ba4c863a92b6e1ec4
Milo Knowles <mknowles@mit.edu>
2017-11-21 19:51:36 -0500

    Add validation / testing augmentation

--- model/pytorch/ensemble.py
+++ model/pytorch/ensemble.py
@@ -3,6 +3,13 @@
 
 # Your team, LaCroixNet, has been registered for the challenge.
 # Your team code (case-sensitive) for submitting to the leaderboard is: fFR7jWuG2XqiAImnJQrN
+
+# Baseline: 71.97
+# (N=0.5, H=0.4, V=0.1) : 72.549
+# (N=0.5, H=0.5, V=0) : 72.459
+# (N=0.6, H=0.4) : 72.5
+# (N=0.333, H=0.333, V=0.333) : 71.139
+
 from __future__ import print_function, division
 import os, sys, time
 
@@ -21,7 +28,7 @@ from miniplaces_capsnet3 import PlacesCapsuleNet
 from miniplaces_dataset import *
 from utils import accuracy, AverageMeter, save_checkpoint, log
 
-BATCH_SIZE = 1
+BATCH_SIZE = 32
 VGG_LOAD_EPOCH = './final/model_best.pth.tar'
 CAPSNET_LOAD_EPOCH = './final/epoch_16.pt'
 
@@ -35,6 +42,12 @@ MODEL1_WEIGHT = 0.7
 MODEL2_WEIGHT = 0.3
 assert(MODEL1_WEIGHT + MODEL2_WEIGHT == 1.0)
 
+# Weights for using image augmentation for validation / testing.
+WEIGHT_NORMAL = 0.333
+WEIGHT_HORIZ = 0.333
+WEIGHT_VERT = 0.333
+
+print('WEIGHT_NORMAL: %f WEIGHT_HORIZ: %f WEIGHT_VERT: %f' % (WEIGHT_NORMAL, WEIGHT_HORIZ, WEIGHT_VERT))
 
 def validate(val_loader, models, print_freq=1):
   for model in models:
@@ -106,6 +119,58 @@ def validate(val_loader, models, print_freq=1):
   return top1_ensemble.avg, top5_ensemble.avg
 
 
+def validate_augmented(val_loader, model, print_freq=1):
+  model.eval()
+
+  print('Starting validation of augmented model!')
+
+  batch_time = AverageMeter()
+  top1_acc = AverageMeter()
+  top5_acc = AverageMeter()
+
+  end = time.time()
+  for i, (image, image_horiz, image_vert, target) in enumerate(val_loader):
+
+    target = target.cuda(async=True)
+    target_var = Variable(target)
+
+    image_var = Variable(image).cuda()
+    image_horiz_var = Variable(image_horiz).cuda()
+    image_vert_var = Variable(image_vert).cuda()
+
+    # Get outputs from each model in ensemble.
+    output_normal = model(image_var)
+    output_horiz = model(image_horiz_var)
+    output_vert = model(image_vert_var)
+
+    output_weighted = WEIGHT_NORMAL * output_normal.data + WEIGHT_HORIZ * output_horiz.data + WEIGHT_VERT * output_vert.data
+
+    # Measure accuracies.
+    prec1, prec5 = accuracy(output_weighted, target, topk=(1,5))
+    top1_acc.update(prec1, image.size(0))
+    top5_acc.update(prec5, image.size(0))
+
+    # Measure time.
+    batch_time.update(time.time() - end)
+    end = time.time()
+
+    print_str = 'Validation: [%d/%d] \t Prec1(aug): %f (%f) Prec5(aug): %f (%f) \n' \
+                % (i, len(val_loader), top1_acc.val[0], top1_acc.avg[0], top5_acc.val[0], top5_acc.avg[0])
+
+    # Print statistacks.
+    if i % print_freq == 0:
+      print(print_str)
+
+  # Print out final statistacks.
+
+  print(print_str)
+  print_str = 'Validation: [%d/%d] \t Prec1(aug): %f (%f) Prec5(aug): %f (%f) \n' \
+                % (i, len(val_loader), top1_acc.val[0], top1_acc.avg[0], top5_acc.val[0], top5_acc.avg[0])
+
+  print('Finished validation!')
+  return top1_acc.avg, top5_acc.avg
+
+
 def generate_submission_ensemble(models, transforms_list, print_freq=100):
 
   # Set up a test loader, which outputs image / filename pairs.
@@ -150,6 +215,46 @@ def generate_submission_ensemble(models, transforms_list, print_freq=100):
 
   print('Finished generating submission!')
 
+def generate_submission_augmented(model, transforms_list, print_freq=100):
+  # Set up a test loader, which outputs image / filename pairs.
+  test_set = MiniPlacesTestSet('../../data/images/test/',
+                               transform=transforms.Compose(transforms_list),
+                               outfile=str(int(time.time())) + 'predictions.txt',
+                               get_flipped=True)
+
+  test_loader = torch.utils.data.DataLoader(test_set, batch_size=1, shuffle=True, num_workers=4)
+
+  model.eval()
+
+  print('Generating submission!')
+  for i, data in enumerate(test_loader):
+      image, image_horizontal_flip, image_vertical_flip, filename = data
+
+      input_normal = torch.autograd.Variable(image).cuda()
+      input_horiz = torch.autograd.Variable(image_horizontal_flip).cuda()
+      input_vert = torch.autograd.Variable(image_vertical_flip).cuda()
+
+      # Get outputs for each transformed image.
+      output_normal = model(input_normal)
+      output_horiz = model(input_horiz)
+      output_vert = model(input_vert)
+
+      output_weighted = WEIGHT_NORMAL * output_normal.data + WEIGHT_HORIZ * output_horiz.data + WEIGHT_VERT * output_vert.data
+
+      _, top5_weighted = output_weighted.topk(5, 1, True, True)
+      top5_weighted = top5_weighted.t()
+      labels_weighted = [top5_weighted[ii][0] for ii in range(5)]
+
+      # Write the top 5 labels as a new line.
+      test_set.write_labels('test/' + filename[0], labels_weighted)
+
+      # Print statistacks.
+      if i % print_freq == 0:
+        print('Finished %d/%d' % (i, len(test_loader)))
+
+  print('Finished generating submission!')
+
+
 def generate_submission(model, transforms_list, print_freq=100):
   # Set up a test loader, which outputs image / filename pairs.
   test_set = MiniPlacesTestSet('../../data/images/test/',
@@ -168,7 +273,7 @@ def generate_submission(model, transforms_list, print_freq=100):
       output = model(input_var)
       _, top5 = output.topk(5, 1, True, True)
       top5 = top5.t()
-      labels = [top5.data[i][0] for i in range(5)]
+      labels = [top5.data[ii][0] for ii in range(5)]
 
       # Write the top 5 labels as a new line.
       test_set.write_labels('test/' + filename[0], labels)
@@ -207,12 +312,14 @@ if __name__ == '__main__':
                     transforms.Normalize(DATA_MEAN, DATA_STD)]
 
   # Uncomment below for validating.
-  # val_set = MiniPlacesDataset(os.path.abspath('./../../data/val.txt'),
-  #                             os.path.abspath('./../../data/images/'),
-  #                             transform=transforms.Compose(transforms_list))
-  # val_loader = torch.utils.data.DataLoader(val_set, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
+  val_set = MiniPlacesDataset(os.path.abspath('./../../data/val.txt'),
+                              os.path.abspath('./../../data/images/'),
+                              transform=transforms.Compose(transforms_list),
+                              get_flipped=True)
+  val_loader = torch.utils.data.DataLoader(val_set, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
 
-  # validate(val_loader, [model_vgg16, model_capsnet], print_freq=10)
+  validate_augmented(val_loader, model_vgg16, print_freq=10)
 
   # Uncomment for generating a submission file.
-  generate_submission(model_vgg16, transforms_list)
+  # generate_submission(model_vgg16, transforms_list)
+  # generate_submission_augmented(model_vgg16, transforms_list)
--- model/pytorch/miniplaces_dataset.py
+++ model/pytorch/miniplaces_dataset.py
@@ -2,11 +2,12 @@ from __future__ import print_function, division
 import os
 import torch
 from PIL import Image
+from PIL.ImageOps import mirror, flip
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
 
 class MiniPlacesDataset(Dataset):
-    def __init__(self, txt_file, root_dir, transform=None):
+    def __init__(self, txt_file, root_dir, transform=None, get_flipped=False):
         """
         Args:
             txt_file (string): Path to the text file which associates image filenames with classes.
@@ -20,6 +21,7 @@ class MiniPlacesDataset(Dataset):
                 self.lines.append(line)
         self.transform = transform
         self.root_dir = root_dir
+        self.get_flipped = get_flipped
         assert(len(self.lines) > 0)
 
         print('Loaded MiniPlacesDataset.')
@@ -37,10 +39,22 @@ class MiniPlacesDataset(Dataset):
 
         image = Image.open(img_path)
 
-        if self.transform:
-            image = self.transform(image)
+        if self.get_flipped:
+            image_horizontal_flip = mirror(image)
+            image_vertical_flip = flip(image)
 
-        return image, label
+            if self.transform:
+                image = self.transform(image)
+                image_horizontal_flip = self.transform(image_horizontal_flip)
+                image_vertical_flip = self.transform(image_vertical_flip)
+                
+            return image, image_horizontal_flip, image_vertical_flip, label
+
+        else:
+            if self.transform:
+                image = self.transform(image)
+
+            return image, label
 
 class MiniPlacesTestSet(Dataset):
     """
@@ -48,12 +62,13 @@ class MiniPlacesTestSet(Dataset):
     The test set only has images, which are in the images_dir.
     There are no labels -- these are provided by the model.
     """
-    def __init__(self, images_dir, transform=None, outfile='./predictions.txt'):
+    def __init__(self, images_dir, transform=None, outfile='./predictions.txt', get_flipped=False):
         self.image_files = os.listdir(images_dir)
         self.image_files.sort()
         self.transform = transform
         self.images_dir = images_dir
         self.outfile = outfile
+        self.get_flipped = get_flipped
         print('Loaded MiniPlaces test set from: %s' % self.images_dir)
 
     def __len__(self):
@@ -62,8 +77,22 @@ class MiniPlacesTestSet(Dataset):
     def __getitem__(self, idx):
         """ Returns a transformed image and filename. """
         image = Image.open(os.path.join(self.images_dir, self.image_files[idx]))
-        if self.transform: image = self.transform(image)
-        return image, self.image_files[idx]
+
+        # Return the image and it's flipped counterparts.
+        if self.get_flipped:
+            image_horizontal_flip = mirror(image)
+            image_vertical_flip = flip(image)
+
+            if self.transform:
+                image = self.transform(image)
+                image_horizontal_flip = self.transform(image_horizontal_flip)
+                image_vertical_flip = self.transform(image_vertical_flip)
+            return image, image_horizontal_flip, image_vertical_flip, self.image_files[idx]
+
+        # Otherwise, just return the single image.
+        else:
+            if self.transform: image = self.transform(image)
+            return image, self.image_files[idx]
 
     def write_labels(self, filename, labels):
         with open(self.outfile, 'a') as f:
